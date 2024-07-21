e5374d57cb1ddf5affcf208b9fa21d2998ab1133
LimAhrim <157202109+LimAhrim@users.noreply.github.com>
2024-07-21 09:23:05 +0900

    Update FgcMode.cpp

--- src/modes/FgcMode.cpp
+++ src/modes/FgcMode.cpp
@@ -27,8 +27,8 @@ void FgcMode::UpdateDigitalOutputs(InputState &inputs, OutputState &outputs) {
     outputs.home = inputs.c_down;
 
     // Stick buttons
-    outputs.rightStickZ = inputs.c_up;
-    outputs.leftStickZ = inputs.c_left;
+    outputs.rightStick3 = inputs.c_up;
+    outputs.leftStick3 = inputs.c_left;
 
     // Right hand bottom row
     outputs.a = inputs.b;
